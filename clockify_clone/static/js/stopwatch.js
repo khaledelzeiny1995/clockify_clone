b1d7bcd4308e5136459829e947b700547c6bc429
khaledelzeiny995 <khaled.elzeiny@lilyandlou.com>
2020-09-20 20:07:19 -0500

    i added the d3.js plus i worked on styling and generating the list

--- clockify_clone/static/js/stopwatch.js
+++ clockify_clone/static/js/stopwatch.js
@@ -1,13 +1,10 @@
 // stop watch js  
 
-
 var secounds = 0,
     mins = 0,
     hours = 0;
 
 var timer;
-
-
 
 
 function start() {
@@ -61,3 +58,35 @@ function lap() {
         document.querySelector('.laps').appendChild(list);
     }
 }
+
+
+
+// bar chart
+// javascript
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500,
+    svgHeight = 300,
+    barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return svgHeight - d
+    })
+    .attr("height", function(d) {
+        return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function(d, i) {
+        var translate = [barWidth * i, 0];
+        return "translate(" + translate + ")";
+    });
